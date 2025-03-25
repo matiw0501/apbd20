797a3b4b5a410dee60163c319c860c4b92fabb4e
Mateusz W <s31224@pjwstk.edu.pl>
2025-03-26 00:39:05 +0100

    prbly no changes

--- ConsoleApp1/ConsoleApp1/ContainerShip.cs
+++ ConsoleApp1/ConsoleApp1/ContainerShip.cs
@@ -2,5 +2,12 @@ namespace ConsoleApp1;
 
 public class ContainerShip
 {
+    private int _maxSpeed;
+    private int _maxCapacity;
+    private int _maxWeight;
+    
+    public List<Contener> conteners;
+
+    
     
 }
